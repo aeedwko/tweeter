7083cad9d962b14b5b52c151fadb059240252d82
Edwin Ko <edwin.mk.ko@gmail.com>
2023-12-17 21:22:43 +0000

    implement html markup and css styling for tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,20 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(() => {
+  // add/remove box shadow when hovered on tweet
+  $(".tweet").on("mouseenter", function() {
+    $(this).addClass("hovered-tweet");
+  }).on("mouseleave", function() {
+    $(this).removeClass("hovered-tweet");
+  });
+
+  // change colour of icon when hovered 
+  $(".tweet i").on("mouseenter", function() {
+    $(this).addClass("hovered-icon");
+  }).on("mouseleave", function() {
+    $(this).removeClass("hovered-icon");
+  });
+});
+
 
