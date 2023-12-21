17ff76bff87e407a23f14322b2639521b1da6a71
Edwin Ko <edwin.mk.ko@gmail.com>
2023-12-21 19:43:25 +0000

    add dynamic tweet rendering

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,6 +4,34 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(() => {
+  // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+  
+  renderTweets(data);
+
   // add/remove box shadow when hovered on tweet
   $(".tweet").on("mouseenter", function() {
     $(this).addClass("hovered-tweet");
@@ -19,4 +47,33 @@ $(document).ready(() => {
   });
 });
 
+const createTweetElement = function(data) {
+  return $(
+    `<article class="tweet">
+  <header>
+    <div>
+      <img src="${data.user.avatars}">
+      <span>${data.user.name}</span>
+    </div>          
+    <span>${data.user.handle}</span>
+  </header>
+  <article>${data.content.text}</article>
+  <footer>
+    <span>${data.created_at}</span>
+    <div>
+      <a href=""><i class="fa-solid fa-flag"></i></a>
+      <a href=""><i class="fa-solid fa-retweet"></i></a>
+      <a href=""><i class="fa-solid fa-heart"></i></a>
+    </div>
+  </footer>
+  </article>  
+`);
+};
 
+const renderTweets = function(tweets) {
+  for (const tweet of tweets) {
+    console.log(tweet);
+    let $tweet = createTweetElement(tweet);
+    $('#tweets-container').append($tweet);
+  }
+};
