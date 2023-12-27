a796e54116572111ecfde6b53b6aefd32766d29c
Edwin Ko <edwin.mk.ko@gmail.com>
2023-12-27 01:14:19 +0000

    add post and get request to server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,33 +4,16 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(() => {
-  // Fake data taken from initial-tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-  
-  renderTweets(data);
+
+  // GET request for all 
+  const loadTweets = () => {
+    $.get("/tweets", (data) => {
+      console.log(data);
+      renderTweets(data);
+    })
+  }
+
+  loadTweets();
 
   // add/remove box shadow when hovered on tweet
   $(".tweet").on("mouseenter", function() {
@@ -45,7 +28,21 @@ $(document).ready(() => {
   }).on("mouseleave", function() {
     $(this).removeClass("hovered-icon");
   });
+
+  $("form").on("submit", function(event) {
+    event.preventDefault();
+    
+    $.post("/tweets", $(this).serialize());
+  });
 });
+
+const renderTweets = function(tweets) {
+  for (const tweet of tweets) {
+    console.log(tweet);
+    let $tweet = createTweetElement(tweet);
+    $('#tweets-container').append($tweet);
+  }
+};
 
 const createTweetElement = function(data) {
   return $(
@@ -68,12 +65,4 @@ const createTweetElement = function(data) {
   </footer>
   </article>  
 `);
-};
-
-const renderTweets = function(tweets) {
-  for (const tweet of tweets) {
-    console.log(tweet);
-    let $tweet = createTweetElement(tweet);
-    $('#tweets-container').append($tweet);
-  }
 };
